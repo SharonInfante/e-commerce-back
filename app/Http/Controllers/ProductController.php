8847c76ec2b768a0007e2ab68a256312dbf021f5
SharonInfante <sharonshabel@gmail.com>
2023-03-16 13:17:01 +0100

    create controller Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    public function index()
+    {
+        $products = Product::all();
+        return view('/', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('addProduct');
+    }
+
+    public function store(Request $request)
+    {
+        $products = new Product();
+
+        $products->product_name = $request->product_name;
+        $products->code = $request->code;
+        $products->price = $request->price;
+        $products->description = $request->description;
+        $products->image = $request->image;
+
+        $products->save();
+
+        return redirect()->route('home', $products);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,38 +7,8 @@
     <title>Document</title>
 </head>
 <body>
-    añadir producto
+    <h1>MI CAMPO</h1>
 
-    <form action="{{route('addSong.store')}}" method="POST">
-
-        @csrf
-
-        <label>
-            Title
-            <input type="text" name="title">
-        </label><br><br>
-        
-
-        <label>
-            Artist
-            <input type="text" name="artist">
-        </label><br><br>
-
-        <label>
-            Genre
-            <input type="text" name="genre">
-        </label><br><br>
-
-        <label>
-            Url Video
-            <input type="text" name="url">
-        </label><br><br>
-
-        <label>
-            Imagen
-            <input type="file" name="image">
-        </label><br><br>
-        <button type="submit">Enviar</button>
-    </form>
+    
 </body>
 </html>
